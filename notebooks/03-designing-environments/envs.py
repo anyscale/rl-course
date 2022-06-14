98fa3141996412d84fbf9e053928c51e123813f5
Mike Gelbart <mgelbart@cs.ubc.ca>
2022-06-14 13:15:09 -0700

    trying reward shaping

--- notebooks/03-designing-environments/envs.py
+++ notebooks/03-designing-environments/envs.py
@@ -72,7 +72,6 @@ class Maze(FrozenPond):
     def is_valid_loc(self, location):
         return 0 <= location[0] <= 3 and 0 <= location[1] <= 3 and not self.holes[location]
 
-
     def render(self):
         for i in range(4):
             for j in range(4):
@@ -129,12 +128,51 @@ class RandomLakeObs(RandomLake):
         obs = np.array(obs, dtype=int) # this line is optional, helps readability of output
         return obs
     
-class RandomLakeObsRew(RandomLakeObs):
+class RandomLakeObsRew(RandomLakeObs): # fails to reach goal, bad reward
     def reward(self):
         return 6-(abs(self.player[0]-self.goal[0]) + abs(self.player[1]-self.goal[1]))
     
-class RandomLakeObsTest(RandomLakeObs):
-    def reward(self):
-        goal_rew = int(self.player == self.goal)
-        hole_rew = 0.1*(1-self.holes[self.player])
-        return goal_rew + hole_rew
+# class RandomLakeObsTest(RandomLakeObs):
+#     def reward(self):
+#         goal_rew = int(self.player == self.goal)
+#         hole_rew = 0.1*(1-self.holes[self.player])
+#         return goal_rew + hole_rew
+
+class RandomLakeObsRew2(RandomLakeObs): # this one should work
+    
+    # COPIED from above, but with the modification that reward can depend on action
+    # not very good code practice, but makes the lesson clearer
+    # could also do some sort of wrapper thing
+    def step(self, action):
+        # Compute the new player location
+        if action == 0:   # left
+            new_loc = (self.player[0], self.player[1]-1)
+        elif action == 1: # down
+            new_loc = (self.player[0]+1, self.player[1])
+        elif action == 2: # right
+            new_loc = (self.player[0], self.player[1]+1)
+        elif action == 3: # up
+            new_loc = (self.player[0]-1, self.player[1])
+        else:
+            raise ValueError("Action must be in {0,1,2,3}")
+        
+        # Update the player location only if you stayed in bounds
+        # (if you try to move out of bounds, the action does nothing)
+        if self.is_valid_loc(new_loc):
+            self.player = new_loc
+        
+        # Return observation/reward/done
+        return self.observation(), self.reward(action), self.done(), {"player" : self.player, "goal" : self.goal}
+    
+    def reward(self, action):
+        reward = 0
+        if action in (0,3): # up, left
+            reward -= 0.2
+        elif action in (1,2): #down, right
+            reward -= 0.1
+        
+        if self.player == self.goal:
+            reward += 1
+            
+        return reward
+    
