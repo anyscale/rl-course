0ff299b1833aeb30a4785b78110cc0ebfe40d837
Mike Gelbart <mgelbart@cs.ubc.ca>
2022-06-16 13:58:00 -0700

    a bunch of experimenting with rewards

--- notebooks/03-designing-environments/envs.py
+++ notebooks/03-designing-environments/envs.py
@@ -2,25 +2,29 @@ import gym
 import numpy as np
 
 class FrozenPond(gym.Env):
-    def __init__(self, env_config=None):
-        self.observation_space = gym.spaces.Discrete(16)
-        self.action_space = gym.spaces.Discrete(4)      
+    def __init__(self, env_config=None, size=4):
+        self.size = size
+        self.observation_space = gym.spaces.Discrete(size*size)
+        self.action_space = gym.spaces.Discrete(size) 
         
     def reset(self):
         self.player = (0, 0) # the player starts at the top-left
-        self.goal = (3, 3)   # goal is at the bottom-right
+        self.goal = (size-1, size-1)   # goal is at the bottom-right
         
-        self.holes = np.array([
-            [0,0,0,0], # FFFF 
-            [0,1,0,1], # FHFH
-            [0,0,0,1], # FFFH
-            [1,0,0,0]  # HFFF
-        ])
+        if self.size == 4:
+            self.holes = np.array([
+                [0,0,0,0], # FFFF 
+                [0,1,0,1], # FHFH
+                [0,0,0,1], # FFFH
+                [1,0,0,0]  # HFFF
+            ])
+        else:
+            raise Exception("Frozen Pond only supports size 4")
         
         return self.observation()
     
     def observation(self):
-        return 4*self.player[0] + self.player[1]
+        return self.size*self.player[0] + self.player[1]
     
     def reward(self):
         return int(self.player == self.goal)
@@ -30,7 +34,7 @@ class FrozenPond(gym.Env):
         # cast from numpy.bool to bool because of the RLlib check_env
     
     def is_valid_loc(self, location):
-        return 0 <= location[0] <= 3 and 0 <= location[1] <= 3
+        return 0 <= location[0] < self.size and 0 <= location[1] < self.size
 
     def step(self, action):
         # Compute the new player location
@@ -54,8 +58,8 @@ class FrozenPond(gym.Env):
         return self.observation(), self.reward(), self.done(), {"player" : self.player, "goal" : self.goal}
     
     def render(self):
-        for i in range(4):
-            for j in range(4):
+        for i in range(self.size):
+            for j in range(self.size):
                 if (i,j) == self.player:
                     print("P", end="")
                 elif (i,j) == self.goal:
@@ -70,11 +74,11 @@ class Maze(FrozenPond):
     def done(self):
         return self.player == self.goal
     def is_valid_loc(self, location):
-        return 0 <= location[0] <= 3 and 0 <= location[1] <= 3 and not self.holes[location]
+        return 0 <= location[0] < self.size and 0 <= location[1] < self.size and not self.holes[location]
 
     def render(self):
-        for i in range(4):
-            for j in range(4):
+        for i in range(self.size):
+            for j in range(self.size):
                 if (i,j) == self.goal:
                     print("G", end="")
                 elif (i,j) == self.player:
@@ -91,7 +95,7 @@ class RandomMaze(Maze):
         self.player = (0, 0) # the player starts at the top-left
         self.goal = (3, 3)   # goal is at the bottom-right
         
-        self.walls = np.random.rand(4, 4) < 0.2
+        self.walls = np.random.rand(self.size, self.size) < 0.2
         self.walls[self.player] = 0
         self.walls[self.goal] = 0
         
@@ -103,34 +107,35 @@ class RandomMaze(Maze):
 class RandomLake(FrozenPond):
     def reset(self):
         self.player = (0, 0) # the player starts at the top-left
-        self.goal = (3, 3)   # goal is at the bottom-right
+        self.goal = (self.size-1, self.size-1)   # goal is at the bottom-right
         
-        self.holes = np.random.rand(4, 4) < 0.2
+        self.holes = np.random.rand(self.size, self.size) < 0.2
         self.holes[self.player] = 0  # no hole at start location
         self.holes[self.goal] = 0    # no hole at goal location
         
         return self.observation()
     
 class RandomLakeObs(RandomLake):
-    def __init__(self, env_config=None):
-        self.observation_space = gym.spaces.MultiDiscrete([2,2,2,2])
-        self.action_space = gym.spaces.Discrete(4)      
+    def __init__(self, env_config=None, size=4):
+        self.size = size
+        self.observation_space = gym.spaces.MultiDiscrete([2]*size)
+        self.action_space = gym.spaces.Discrete(self.size)      
     
     def observation(self):
         i, j = self.player
 
         obs = []
-        obs.append(1 if j==0 else self.holes[i,j-1]) # left
-        obs.append(1 if i==3 else self.holes[i+1,j]) # down
-        obs.append(1 if j==3 else self.holes[i,j+1]) # right
-        obs.append(1 if i==0 else self.holes[i-1,j]) # up
+        obs.append(1 if j==0           else self.holes[i,j-1]) # left
+        obs.append(1 if i==self.size-1 else self.holes[i+1,j]) # down
+        obs.append(1 if j==self.size-1 else self.holes[i,j+1]) # right
+        obs.append(1 if i==0           else self.holes[i-1,j]) # up
         
         obs = np.array(obs, dtype=int) # this line is optional, helps readability of output
         return obs
     
 class RandomLakeObsRew(RandomLakeObs): # fails to reach goal, bad reward
     def reward(self):
-        return 6-(abs(self.player[0]-self.goal[0]) + abs(self.player[1]-self.goal[1]))
+        return (self.size*2-2)-(abs(self.player[0]-self.goal[0]) + abs(self.player[1]-self.goal[1]))
     
 # class RandomLakeObsTest(RandomLakeObs):
 #     def reward(self):
@@ -156,20 +161,26 @@ class RandomLakeObsRew2(RandomLakeObs): # this one should work
         else:
             raise ValueError("Action must be in {0,1,2,3}")
         
+        rew = 0
+        
         # Update the player location only if you stayed in bounds
         # (if you try to move out of bounds, the action does nothing)
         if self.is_valid_loc(new_loc):
             self.player = new_loc
+        else:
+            rew -= 0.1
+            
         
         # Return observation/reward/done
-        return self.observation(), self.reward(action), self.done(), {"player" : self.player, "goal" : self.goal}
+        return self.observation(), self.reward(action) + rew, self.done(), {"player" : self.player, "goal" : self.goal}
     
     def reward(self, action):
         reward = 0
-        if action in (0,3): # left, up
-            reward -= 0.02
-        elif action in (1,2): # down, right
-            reward -= 0.01
+        if self.holes[self.player]:
+            reward -= 0.1
+            
+        # if action in (0,3):
+            # reward -= 0.01
         
         if self.player == self.goal:
             reward += 1
