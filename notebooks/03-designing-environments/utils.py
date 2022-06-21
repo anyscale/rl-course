19316366c457b94e08f497c18e5f8a7fe652b978
Mike Gelbart <mgelbart@cs.ubc.ca>
2022-06-21 13:50:39 -0700

    fix some more rendering highlight

--- notebooks/03-designing-environments/utils.py
+++ notebooks/03-designing-environments/utils.py
@@ -12,3 +12,32 @@ def query_policy(trainer, env, obs, actions=None):
         actions = [0,1,2,3]
     probs = np.exp(dist.logp(torch.from_numpy(np.array(actions))).detach().numpy())
     return probs
+
+# TODO: don't repeat this code from module 1 utils
+def my_render(self):
+    
+    if self.lastaction is not None:
+        print(f"  ({['Left', 'Down', 'Right', 'Up'][self.lastaction]})")
+    else:
+        print("")
+    
+    desc = self.desc.tolist()
+
+    row, col = self.s // self.ncol, self.s % self.ncol
+    desc = [[c.decode("utf-8") for c in line] for line in desc]
+
+    for i, line in enumerate(desc):
+        for j, c in enumerate(line):
+            if (i,j) == (row, col):
+                print("P", end="")
+            elif desc[i][j] == "G":
+                print("G", end="")
+            elif desc[i][j] == "H":
+                print("O", end="")
+            else:
+                print(".", end="")
+        print()
+
+def fix_frozen_lake_render(env):
+    env.render = type(env.render)(my_render, env)
+    
