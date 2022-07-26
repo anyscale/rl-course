1287a5a49a5dd6de2a422d4f0cacc307774f4050
Mike Gelbart <mgelbart@cs.ubc.ca>
2022-07-26 12:45:44 -0700

    module 3 to ray 2

--- notebooks/03-designing-environments/utils.py
+++ notebooks/03-designing-environments/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from ray.rllib.models.preprocessors import get_preprocessor 
+from ray.rllib.algorithms.ppo import PPOConfig
 
 def query_policy(trainer, env, obs, actions=None):
     policy = trainer.get_policy()
@@ -13,10 +14,18 @@ def query_policy(trainer, env, obs, actions=None):
     probs = np.exp(dist.logp(torch.from_numpy(np.array(actions))).detach().numpy())
     return probs
 
-lake_default_config = {
-    "framework"             : "torch",
-    "create_env_on_driver"  : True,
-    "seed"                  : 0,
-    "horizon"               : 100,
-    "model"                 : {"fcnet_hiddens" : [32, 32]}
-}
+# lake_default_config = {
+#     "framework"             : "torch",
+#     "create_env_on_driver"  : True,
+#     "seed"                  : 0,
+#     "horizon"               : 100,
+#     "model"                 : {"fcnet_hiddens" : [32, 32]}
+# }
+
+lake_default_config = (
+    PPOConfig()\
+    .framework("torch")\
+    .rollouts(create_env_on_local_worker=True, horizon=100)\
+    .debugging(seed=0, log_level="ERROR")\
+    .training(model={"fcnet_hiddens" : [32, 32]})
+)
